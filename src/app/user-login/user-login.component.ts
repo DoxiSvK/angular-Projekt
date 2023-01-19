3a8c7d3afdb3f63f5770870d98a397331b3d2cee
DoxiSvK <91240836+DoxiSvK@users.noreply.github.com>
2023-01-19 19:58:07 +0100

    1 file modified

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -2,37 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-login',
-  templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+selector: 'app-user-login',
+templateUrl: './user-login.component.html',
+styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
-  signupUsers:any[]=[];
-  signupObj:any={
-    userName:'',
-    email:'',
-    password:''
-  };
-  loginObj:any={
-    userName:'',
-    password:''
-  };
+loginObj:any={
+userName:'',
+password:''
+};
 
+constructor(private router: Router ) { }
 
-  constructor(private router: Router ) { }
+ngOnInit(): void {
 
-  ngOnInit(): void {
-    const localData=localStorage.getItem('signupUsers');
-    if(localData!=null){
-      this.signupUsers=JSON.parse(localData)
-    }
-  }
-  onLogin(){
-    const isUserExist=(m=>'admin'==this.loginObj.userName && 'angular'==this.loginObj.password);
-    if (isUserExist!=undefined){
-      this.router.navigate(['student']);
-    } else {
-      alert('Zlé meno alebo heslo');
-    }
-  }
+}
+
+onLogin(){
+const isUserExist=(m=>'Boss'==this.loginObj.userName && 'boss'==this.loginObj.password);
+if (isUserExist!=undefined){
+this.router.navigate(['student']);
+} else {
+alert('Zlé meno alebo heslo');
+}
+}
 }

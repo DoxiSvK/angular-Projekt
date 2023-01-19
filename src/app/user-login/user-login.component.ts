1c51eac10de1a221b80e5bcccd0e8d28cdd80a18
DoxiSvK <91240836+DoxiSvK@users.noreply.github.com>
2023-01-19 20:15:36 +0100

    1 file modified

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -2,28 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-selector: 'app-user-login',
-templateUrl: './user-login.component.html',
-styleUrls: ['./user-login.component.css']
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
-loginObj:any={
-userName:'',
-password:''
-};
+  loginObj: any = {
+    userName: '',
+    password: ''
+  };
 
-constructor(private router: Router ) { }
+  constructor(private router: Router ) { }
 
-ngOnInit(): void {
+  ngOnInit(): void { }
 
-}
-
-onLogin(){
-const isUserExist=(m=>'Boss'==this.loginObj.userName && 'boss'==this.loginObj.password);
-if (isUserExist!=undefined){
-this.router.navigate(['student']);
-} else {
-alert('Zlé meno alebo heslo');
-}
-}
+  onLogin() {
+    if (this.loginObj.userName === 'boss' && this.loginObj.password === 'boss1') {
+      this.router.navigate(['student']);
+    } else {
+      alert('Zlé meno alebo heslo');
+    }
+  }
 }
